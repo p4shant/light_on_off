203220637943b6221da548bcff39fad9447b971e
prashant <p4shantt@gmail.com>
2023-04-18 00:17:08 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("turn_on").onclick=function(event){
+    // event.preventDefault();
+    console.log("hi")
+    document.getElementById('myimage').src="images/image1.jpg";
+}
+document.getElementById("turn_off").onclick=function(event){
+    // event.preventDefault();
+    document.getElementById('myimage').src="images/image2.jpg"
+}
